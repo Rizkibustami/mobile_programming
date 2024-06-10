1db86206acf878f7b5d3695ca5580ff998a14939
Rizkibustami <abustami23@gmail.com>
2024-06-10 19:10:14 +0700

    tugas pertemuan 6

--- tugas_tugas/lib/main.dart
+++ tugas_tugas/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '/ui/poli_page.dart';
 import '/ui/pegawai_page.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tugas Pertemuan 5',
+      title: 'Tugas Pertemuan 6',
       debugShowCheckedModeBanner: false,
       home: PegawaiPage(),
     );
--- tugas_tugas/lib/ui/pegawai_page.dart
+++ tugas_tugas/lib/ui/pegawai_page.dart
@@ -1,53 +1,41 @@
 import 'package:flutter/material.dart';
+import '../model/pegawai.dart';
+import '../model/pasien.dart';
 import 'pegawai_detail.dart';
 import 'pasien_detail.dart';
-import '../model/pegawai.dart';
-import '../model/pasien.dart';   
-   
-class PegawaiPage extends StatefulWidget {   
+import 'pegawai_item.dart';
+import 'pasien_item.dart';
+import 'pegawai_form.dart';
+
+class PegawaiPage extends StatefulWidget {
   const PegawaiPage({super.key});
-  
+
   @override
   State<PegawaiPage> createState() => _PegawaiPageState();
-}   
-    
-class _PegawaiPageState extends State<PegawaiPage> {   
-  @override   
-  Widget build(BuildContext context) {  
-    return Scaffold(        
-      appBar: AppBar(title: const Text("Data Rumah Sakit SADEWA")),   
-      body: ListView(   
-        children: [   
-          GestureDetector(  
-            child: Card( 
-              child: ListTile(   
-                title: const Text("Pegawai"),   
-            ),   
-          ),   
-          onTap: () {   
-            Pegawai pegawai = new Pegawai(namaPegawai: "Nama Pegawai");   
-            Navigator.push(   
-              context,   
-              MaterialPageRoute(  
-                builder: (context) => PegawaiDetail(pegawai: pegawai)));   
-          },   
-        ),            
-          GestureDetector(  
-            child: Card( 
-              child: ListTile(   
-                title: const Text("Pasien"),   
-            ),   
-          ),   
-          onTap: () {   
-            Pasien pasien = new Pasien(namaPasien: "Nama Pasien");   
-            Navigator.push(   
-              context,   
-              MaterialPageRoute(  
-                builder: (context) => PasienDetail(pasien: pasien)));   
-              },   
-            ),         
-          ],   
-        ), 
-      ); 
-    }   
-  }   
+}
+
+class _PegawaiPageState extends State<PegawaiPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Data Rumah Sakit SADEWA"),
+        actions: [
+          GestureDetector(
+            child: const Icon(Icons.add),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PegawaiForm()));
+            },
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          PegawaiItem(pegawai: Pegawai(namaPegawai: "Pegawai")),
+          PasienItem(pasien: Pasien(namaPasien: "Pasien")),
+        ],
+      ),
+    );
+  }
+}

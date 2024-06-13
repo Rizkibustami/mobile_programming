d264dec067b8d29dee22d98e7ef33d2110655e11
Rizkibustami <abustami23@gmail>
2024-06-13 15:49:28 +0700

    tugas pertemuan 7

--- tugas_tugas/lib/ui/pegawai_detail.dart
+++ tugas_tugas/lib/ui/pegawai_detail.dart
@@ -1,171 +1,57 @@
-import 'package:flutter/material.dart';   
-import '../model/pegawai.dart';   
-    
- class PegawaiDetail extends StatefulWidget {   
-   final Pegawai pegawai;   
-    
-   const PegawaiDetail({super.key, required this.pegawai});   
-    
-   @override   
-   State<PegawaiDetail> createState() => _PegawaiDetailState();   
- }   
-    
- class _PegawaiDetailState extends State<PegawaiDetail> {   
-   @override   
-   Widget build(BuildContext context) {   
-     return Scaffold(
-      appBar: AppBar(title: Text("Detail Pegawai")),   
+import 'package:flutter/material.dart';
+import 'pegawai_update_form.dart';
+import '../model/pegawai.dart';
+
+class PegawaiDetail extends StatefulWidget {
+  final Pegawai pegawai;
+
+  const PegawaiDetail({super.key, required this.pegawai});
+  @override
+  State<PegawaiDetail> createState() => _PegawaiDetailState();
+}
+
+class _PegawaiDetailState extends State<PegawaiDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Detail Pegawai")),
       body: Column(
         children: [
-          SizedBox(height: 12),
-          Text(
-            "ID Pegawai : ${widget.pegawai.idPegawai}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
-          Text(
-            "NIP : ${widget.pegawai.nip}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
+          SizedBox(height: 20),
           Text(
             "Nama Pegawai : ${widget.pegawai.namaPegawai}",
-            style: TextStyle(fontSize: 12),
+            style: TextStyle(fontSize: 20),
           ),
-          SizedBox(height: 12),   
-          Row(   
+          SizedBox(height: 20),
+          Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Tanggal Lahir : ${widget.pegawai.tanggal_lahir}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Nomor Telepon : ${widget.pegawai.nomor_telepon}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Email : ${widget.pegawai.email}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
-            ],
-          ),
-          SizedBox(height: 12),
-          Text(
-            "Password : ${widget.pegawai.password}",
-            style: TextStyle(fontSize: 12),
-          ),
-          SizedBox(height: 12),   
-          Row(   
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: const Text("Ubah")),
-              ElevatedButton(   
-                onPressed: () {},   
-                style: 
-                    ElevatedButton.styleFrom(backgroundColor: Colors.red),   
-                child: const Text("Hapus")),
+              _tombolUbah(),
+              _tombolHapus(),
             ],
           )
-        ],   
-      ),   
+        ],
+      ),
     );
-  }   
+  }
+
+  _tombolUbah() {
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      PegawaiUpdateForm(pegawai: widget.pegawai)));
+        },
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+        child: const Text("Ubah"));
+  }
+
+  _tombolHapus() {
+    return ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+        child: const Text("Hapus"));
+  }
 }
